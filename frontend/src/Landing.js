a538a69df35b25d0a891d308ab21398e028c0157
Thien Pham <45925109+th13n-h4ck3r@users.noreply.github.com>
2024-02-19 16:09:16 -0600

    login page added

---
+++ frontend/src/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div className="landing-container">
+            <h1>Welcome to Your Website</h1>
+            <p>Discover amazing things here!</p>
+        </div>
+    );
+};
+
+export default Landing;
