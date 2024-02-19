d7e4c6c8fd30ba3c68a383572fd2e5a45cc3f824
Russell <russellangfarinha@gmail.com>
2024-02-19 17:07:08 -0600

    simple navbar setup ;o
    need to start on css and give each item it's function

---
+++ frontend/src/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="nav-container">
+            <nav class="header-nav">
+                <ul class="header-ul">
+                    <li>Home</li>
+                    <li>Data Table</li>
+                    <li>History</li>
+                    <li>Profile</li>
+                    <li>Logout</li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../navbar';
 
 const Landing = () => {
     return (
         <div className="landing-container">
+            <Navbar />
             <h1>Welcome to Your Website</h1>
             <p>Discover amazing things here!</p>
         </div>
