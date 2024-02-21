bed5bb9576c8880eaf2ee31b1715642297c4b807
Russell <russellangfarinha@gmail.com>
2024-02-21 00:52:25 -0600

    asdf

--- frontend/src/navbar.js
+++ frontend/src/navbar.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles/Navbar.css';
 
 const Navbar = () => {
+    const [isActive, setActive] = useState("false");
+    const ToggleClass = () => {
+        setActive(!isActive);
+    };
     return (
-        <div className="nav-container">
-            <nav class="header-nav">
-                <ul class="header-ul">
-                    <li>Home</li>
-                    <li>Data Table</li>
-                    <li>History</li>
-                    <li>Profile</li>
-                    <li>Logout</li>
+        <div className="container">
+            <nav className="nav">
+                <a href="/home" className='site-title'>Group21</a>
+                <ul>
+                    <li><a href='/home' onClick={isActive ? "active" : null}>Home</a></li>
+                    <li><a href='/data-table' onClick={isActive ? "active" : null}>Data Table</a></li>
+                    <li><a href='/history' onClick={isActive ? "active" : null}>History</a></li>
+                    <li><a href='profile' onClick={isActive ? "active" : null}>Profile</a></li>
+                    <li><a href='/logout' onClick={isActive ? "active" : null}>Logout</a></li>
                 </ul>
             </nav>
         </div>
--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Navbar from '../navbar';
+import Navbar from '../Navbar';
+import '../styles/Landing.css';
 
 const Landing = () => {
     return (
