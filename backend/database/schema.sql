953780c68afc0681ddddbc12bb2e6238f18305da
Russell <11906132+ash14545@users.noreply.github.com>
2024-03-14 13:08:48 -0500

    asdf

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -11,9 +11,22 @@ CREATE TABLE users (
         password char(44) not null check(password regexp '^[a-zA-Z0-9+/=]{44}$'),
         primary key(username)
 );
+CREATE TABLE profile (
+        client_username  varchar(16) NOT NULL CHECK(client_username REGEXP '^[a-zA-Z0-9]{3,}$'),
+        full_name varchar(50),
+        email varchar(50) NOT NULL CHECK(email REGEXP '^[[:alnum:]]+([\.-]?[[:alnum:]]+)*@\[[:alnum:]]+([\.-]?[[:alnum:]]+)*(\.[[:alnum:]]{2,3})+$'),
+        address1 varchar(80),
+        address2 varchar(80),
+        city varchar(50),
+        state char(2),
+        zipcode char(5) CHECK(zipcode IS NULL OR (zipcode REGEXP '^[0-9]{5}$')),
+        phone char(10) NOT NULL CHECK(phone REGEXP '[0-9]{10}'),
+        PRIMARY KEY(client_username),
+        FOREIGN KEY(client_username) REFERENCES users(username)
+);
 commit;
 -- Create a role 'app' and login for the backend to use on the database
 create user 'app' identified by 'test_password';
 -- Granting necessarily data-modification privileges to 'app'
 grant insert, delete, select, update on cosc4353group21.* to 'app';
-commit;
+commit;c
