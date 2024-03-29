f3a2034844d03f6d4f6a37cdde3aa835501884dd
raycolli <arrachelcollier@gmail.com>
2024-03-29 17:05:47 -0500

    update history

--- frontend/src/pages/QuoteHistory.js
+++ frontend/src/pages/QuoteHistory.js
@@ -1,14 +1,41 @@
-import React from "react";
+import { React, useEffect, useState } from 'react'
 import "../styles/QuoteHistory.css";
 import Navbar from "./Navbar";
 import Footer from './Footer';
+
+//updates the history
+import { clientWithAuth } from '../services/axiosClient'
+import { useNavigate } from 'react-router-dom'
 
 const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
 })
 
-const QuoteHistory = ({ quotes }) => {
+  const QuoteHistory = () => {
+
+  const navigate = useNavigate();
+  const [ quotes, setQuotes ] = useState([]);
+  const username = localStorage.getItem('username');
+  const token = localStorage.getItem('token');
+
+  
+  useEffect(() => {
+    clientWithAuth(token).get('/history/'+username).then((response) => {
+      console.log(response.data.quotes);
+      setQuotes(response.data.quotes);
+    }).catch((error) => {
+      if (error.response && error.response.status === 400)
+      {
+        navigate('/login');
+      }
+      alert('Quote server is down, try again later');
+      navigate('/');
+    });
+  }, [])
+  
+
+
   return (
     <div className="history-page">
       <div className="nav">
